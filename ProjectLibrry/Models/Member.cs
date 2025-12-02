e9b1f8ef1d48cae9679b9567e5fd29fcf3b20325
Poul Henriksen <pohe@zealand.dk>
2025-12-02 09:53:08 +0100

    Luop added Member

---
+++ ProjectLibrry/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrry.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }
+    }
+}
